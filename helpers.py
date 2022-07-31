e29a90b43c4f6aee2a9856fe417ea261b172d2d2
sngmng6506 <sngmng6506@gmail.com>
2022-07-31 21:14:31 +0900

    1

--- helpers.py
+++ helpers.py
@@ -1,25 +1,36 @@
 input_url_helper="""
 MDTextField:
-     pos_hint: {'center_x':0.5,'center_y':0.5}   
+     pos_hint: {'center_x':0.55,'center_y':0.4}   
      size_hint_x:None 
-     width:150
+     width:dp(190)
      theme_text_color:'Custom'
      text_color: (41/255.0, 41/255.0, 41/255.0, 1)
+     #icon_left: 'youtube'
      
 """
 youtube_icon_btn_helper="""
 MDIconButton:
     icon:'youtube'
-    pos_hint:{'center_x':0.15 , 'center_y':0.5}
-    user_font_size:"32sp"
+    pos_hint:{'center_x':0.15 , 'center_y':0.4}
+    user_font_size:dp(28)
     theme_text_color:'Custom'
     text_color:(196/255.0, 48/255.0, 43/255.0, 1)
     on_release: app.open_youtube()
 """
+soundcloud_icon_btn_helper="""
+MDIconButton:
+    icon:'soundcloud'
+    pos_hint:{'center_x':0.15 , 'center_y':0.3}
+    user_font_size:dp(28)
+    theme_text_color:'Custom'
+    text_color:(240/255.0, 89/255.0, 34/255.0, 1)
+    on_release: app.open_youtube()
+"""
+
 send_flat_btn_helper = """
 MDRectangleFlatButton:
     text:'Send'
-    pos_hint:{'center_x' : 0.5,'center_y':0.3}
+    pos_hint:{'center_x' : 0.5,'center_y':0.2}
     font_size:"14sp"
     #increment_width: "16dp"
     on_release: app.send_url()
@@ -34,23 +45,55 @@ MDLabel:
 """
 connect_label_helper = """
 MDLabel:
-    text:'temp' 
-    pos_hint:{'center_x':0.75,'center_y':.725}
+    text:'Now playing...' 
+    pos_hint:{'center_x':0.55,'center_y':.78}
     theme_text_color:'Custom'
     text_color:(125/255.0, 125/255.0, 125/255.0, 1)
     font_style:'Caption'
 """
 
 cafe_list_dropdown_helper = '''
-<IconListItem>
 
-    IconLeftWidget:
-        icon: root.icon
-MDScreen
-
+MDScreen:
+        
     MDDropDownItem:
         id: drop_item
-        pos_hint: {'center_x': .5, 'center_y': .8}
-        text: 'Connect to Wi-Fi in the cafe'
+        pos_hint: {'center_x': .5, 'center_y': .9}
+        text: 'Connect to Wi-Fi'
         on_release: app.menu.open()
+'''
+
+Fab_helper = """
+MDScreen:
+    MDFloatingActionButtonSpeedDial:
+        callback:app.callback
+        data: app.data
+        root_button_anim: True
+        hint_animation : False
+"""
+Thumbnail_helper = '''
+MDScreen:
+
+    MDSmartTile:
+        radius: 24
+        box_radius: [0, 0, 24, 24]
+        box_color: 1, 1, 1, 0
+        overlap:False
+        source: "test.jpg"
+        pos_hint: {"center_x": .5, "center_y": .6}
+        size_hint: None, None
+        size: "240dp", "180dp"
+
+
+        MDIconButton:
+            icon: "heart-outline"
+            theme_icon_color: "Custom"
+            icon_color: 1, 0, 0, 1
+            pos_hint: {"center_y": .5}
+            on_release: self.icon = "heart" if self.icon == "heart-outline" else "heart-outline"
+
+        MDLabel:
+            text: "test and heart"
+            bold: True
+            color: 0,0,0,1
 '''
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from kivymd.app import MDApp  #0.104.2
 from kivymd.uix.screen import Screen
 from kivymd.uix.label import MDLabel,MDIcon
-from kivymd.uix.button import MDRectangleFlatButton,MDIconButton
+from kivymd.uix.button import MDRectangleFlatButton,MDIconButton,MDFloatingActionButtonSpeedDial
 from kivymd.uix.textfield import MDTextField
 from kivymd.uix.menu import MDDropdownMenu
 from kivy.lang import Builder
@@ -9,8 +9,15 @@ from helpers import *
 from kivy.metrics import dp
 from client_socket import *
 import webbrowser
-
+from kivy.properties import OptionProperty,NumericProperty
 from kivy.core.window import Window # << 모바일은 화면비율 자동으로 바뀌나?
+from kivymd.uix.templates import ScaleWidget
+from kivymd.toast import toast
+import time
+import threading
+from queue import Queue
+from kivy.clock import Clock
+from kivymd.uix.dialog import MDDialog
 
 
 
@@ -19,14 +26,14 @@ class JukeApp(MDApp):
         super().__init__(**kwargs)
         ## cafe_list_dropdownMenu
         self.cafe_list = Builder.load_string(cafe_list_dropdown_helper)
-        Registered_WiFi = {'ediya': '172.30.1.11', 'home': '172.30.1.5'}  ## << 카페,와이파이 추가되면 여기만 변경
+        self.Registered_WiFi = {'ediya': '172.30.1.34', 'home': '172.30.1.25'}
         menu_items = [
             {
                 "viewclass": "OneLineListItem",
                 "text": i,
                 "height": dp(56),
                 "on_release": lambda x= i : self.set_item(x),
-            } for i in Registered_WiFi.keys()
+            } for i in self.Registered_WiFi.keys()
         ]
         self.menu = MDDropdownMenu(
             caller=self.cafe_list.ids.drop_item,
@@ -35,14 +42,21 @@ class JukeApp(MDApp):
             width_mult=4,
         )
         self.menu.bind()
-        
+
+        #FAB
+        self.data = {
+            "a": 'heart',
+            'b': 'help',
+        }
         ## WiFi 상태에 따른 메뉴창 선택됨
-        for key,value in Registered_WiFi.items():
-            if socket.gethostbyname(socket.gethostname()) == value:
+        init_IP = socket.gethostbyname(socket.gethostname())
+        for key,value in self.Registered_WiFi.items():
+            if init_IP == value:
                 print("now u r in " + key)
                 self.set_item(key)
 
-
+        ## help - dialog
+        self.dialog = None
 
     def set_item(self, text_item):
         self.cafe_list.ids.drop_item.set_item(text_item)
@@ -58,33 +72,98 @@ class JukeApp(MDApp):
     def build(self):
         Window.size = [720/2.5, 1600/2.5]
         self.theme_cls.primary_palette="DeepPurple"
-        self.theme_cls.primary_hue = "600"
+        self.theme_cls.primary_hue = "300"
         #self.theme_cls.theme_style = "Dark"  # 옵션가서 설정가능하게하면좋을듯 / Light or Dark
 
         ##### Builders
         self.input_url = Builder.load_string(input_url_helper)
         send_flat_btn = Builder.load_string(send_flat_btn_helper)
         youtube_icon_btn = Builder.load_string(youtube_icon_btn_helper)
-        contact_label = Builder.load_string((contact_label_helper))
+        #contact_label = Builder.load_string((contact_label_helper))
         connect_label = Builder.load_string((connect_label_helper))
+        fab = Builder.load_string(Fab_helper)
+        thumbnail = Builder.load_string(Thumbnail_helper)
+        #soundcloud_icon_btn=Builder.load_string(soundcloud_icon_btn_helper)
+        #Builder.load_file('main.kv')
 
         ##### add_widget
         self.screen = Screen()
-        self.screen.add_widget(contact_label)
+        #self.screen.add_widget(contact_label)
         self.screen.add_widget(youtube_icon_btn)
         self.screen.add_widget(send_flat_btn)
         self.screen.add_widget(self.input_url)
         self.screen.add_widget(self.cafe_list)
-        self.screen.add_widget((connect_label))
+        self.screen.add_widget(connect_label)
+        self.screen.add_widget(fab)
+        self.screen.add_widget(thumbnail)
+        #self.screen.add_widget(soundcloud_icon_btn)
+
+        #thread
+        self.temp = False
+
+        thread = self.go(self.func)
+        thread.start()
+
         return self.screen
 
+    def callback(self,instance):
+        if instance.icon == 'help':
+            self.show_alert_dialog()
+        if instance.icon == 'heart':
+            pass
+
+    def show_alert_dialog(self):
+        if not self.dialog:
+            self.dialog = MDDialog(
+        text="1.매장 와이파이 연결 \n"
+             "2.유튜브 URL 입력 \n"
+             "3.SEND \n"
+             "\n"
+             "contact) sngmng@naver.com",
+        radius=[20, 7, 20, 7],
+        )
+        self.dialog.open()
+
+    def go(self,target):
+        thread = threading.Thread(target=target)
+        return thread
+
+    def func(self): ##네트워크변경감지를위한 스레딩
+        #Registered_WiFi = {'ediya': '172.30.1.34', 'home': '172.30.1.25'}  ## << 카페,와이파이 추가되면 여기 변경
+        init_IP = socket.gethostbyname(socket.gethostname())
+
+        #네트워크변경계속감지 : IP변경안되면 pass 변경되면 다시목록탐색 -> 반복
+        while self.run:
+
+            time.sleep(1)
+            print("네트워크변경감지중")
+            current_IP = socket.gethostbyname(socket.gethostname())
+            if init_IP == current_IP:
+
+
+                print("네트워크동일")
+                pass
+            else:
+                print("네트워크변경!")
+                init_IP = current_IP
+                for key, value in self.Registered_WiFi.items():
+                    if init_IP == value:
+                        print("u r in " + key)
+                        self.set_item(key)
+
+                    else: #toast : you got the wrong WiFi
+                        #self.temp = not self.temp
+                        pass
 
 
 
 
-if __name__ == '__main__':
+
+def Appstart():
     JukeApp().run()
 
+if __name__ == '__main__':
+    Appstart()
 
 
 
